87f18d3eac4405c66b73a83203a8b91b550e7dcf
Kohei Tamura <ktamura.biz.80@gmail.com>
2022-06-20 21:47:56 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit as st
-x=st.radio("What's your favorite movie genre", ('Comedy', 'Drama', 'Documentary'))
+x=st.radio("What's your favorite movie genre", ('Comedy', 'Drama', 'Documentary', 'Fiction'))
 x
